f58ccd270cbd924cbf2604deffdcbf19ce1da307
Tariq Magsi <tariqmagsi125@gmail.com>
2022-05-24 02:50:01 +0200

    download executer basic python code

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import threading
+
+#creating list of threads
+threads = []
+
+def main():
+    vendors_path = './vendors'
+    for file in os.listdir(vendors_path):
+        if file.endswith(".py"):
+            #creating thread
+            process = threading.Thread(target = exec(open("./" + vendors_path + "/" + file).read()))
+            #starting thread
+            process.start()
+            #appending thread to a list
+            threads.append(process)
+            continue
+        else:
+            continue
+    
+    #waiting until thread n completely executed
+    for process in threads:
+        process.join()
+
+
+main()
---
+++ vendors/netgear.py
@@ -0,0 +1,5 @@
+
+def main():
+    print("netgear py")
+
+main()
