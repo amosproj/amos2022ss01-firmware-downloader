22cfc92e5c46ce6a726302e59863dc4a28730b01
Tariq Magsi <tariqmagsi125@gmail.com>
2022-06-07 12:22:20 +0500

    test_check_duplicates added
    Signed-off-by: Tariq Magsi <tariqmagsi125@gmail.com>

--- check_duplicates.py
+++ check_duplicates.py
@@ -6,23 +6,13 @@ def check_duplicates(firmware_data):
     #db connection
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    print(firmware_data["Manufacturer"])
-    if(len(firmware_data["Version"]) > 0):
-        #data selection query from
-        try:
-            cursor.execute("select * from FWDB WHERE Manufacturer='" + firmware_data["Manufacturer"] + "' AND Modelname='" + firmware_data["Modelname"] + "' AND Version = '" + firmware_data["Version"] + "'")
-        except sqlite3.Error as er:
-            print('SQLite error: %s' % (' '.join(er.args)))
-    else:
-        try:
-            cursor.execute("select * from FWDB WHERE Manufacturer='" + firmware_data["Manufacturer"] + "' AND Modelname='" + firmware_data["Modelname"] + "'")
-        except sqlite3.Error as er:
-            print('SQLite error: %s' % (' '.join(er.args)))
-
+    try:
+        cursor.execute("select * from FWDB WHERE Manufacturer='" + firmware_data["Manufacturer"] + "' AND Modelname='" + firmware_data["Modelname"] + "' AND Version = '" + firmware_data["Version"] + "'")
+    except sqlite3.Error as er:
+        print('SQLite error: %s' % (' '.join(er.args)))
 
     data_list = cursor.fetchall()
 
-    print(data_list)
     conn.close()
     if(len(data_list) > 0):
         return True
