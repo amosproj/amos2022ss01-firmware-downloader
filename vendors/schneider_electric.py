b8f7ea96992f9dd0355f9c36f428cb9cf182ec81
root <bilalasgharaziz@gmail.com>
2022-06-08 02:33:48 +0000

    Added implmementation for schneider electric using API

--- vendors/schneider_electric.py
+++ vendors/schneider_electric.py
@@ -1,3 +1,6 @@
+import math
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -11,6 +14,41 @@ def download_file(url, file_path_to_save):
         raise ValueError("Invalid Url or file not found")
     with open(file_path_to_save, "wb") as f:
         f.write(resp.content)
+
+def se_get_total_firmware_count(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    items = soup.find_all("label", class_="dn-check dn-selected")
+    for item in items:
+        if item.get("for") == "docTypeFilters-1555893":
+            innerHtml =item.decode_contents()
+            numbers = re.findall(r'\b\d+\b', innerHtml)
+            count = int(numbers[0])
+            print(f"Found total {count} firmwares")
+            return count
+
+def get_firmware_data_using_api(url, fw_count, fw_per_page):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    total_pages = math.ceil(fw_count/fw_per_page)
+    fw_list = list()
+    for page in range(1, total_pages+1):
+
+        req_body= {
+            "data": f"docType=1555893-Firmware_COMMA_language=en_GB-English_COMMA_sortByField=Document_Date_New_COMMA_itemsPerPage={fw_per_page}_COMMA_pageNumber={page}_COMMA_COMMA_keyword=",
+            "buisnessId": ""
+        }
+        response = requests.post(url, data=req_body, headers=headers)
+        if response.status_code != 200:
+            raise ValueError(f"Invalid API response with status_code = {response.status_code}")
+        if page != total_pages:
+            print(f"Received metadata for {page*fw_per_page}/{fw_count}")
+        else:
+            print(f"Received metadata for all {fw_count}/{fw_count} firmwares")
+        fw_list += response.json()["docList"]
+    return fw_list
+
+def transform_metadata_format_ours(raw_data, local_storage_dir="."):
+    pass
 
 def se_firmaware_parser(url, folder):
     dest = os.path.join(os.getcwd(), folder)
@@ -35,4 +73,9 @@ def se_firmaware_parser(url, folder):
 if __name__ == "__main__":
     url = "https://www.se.com/ww/en/download/doc-group-type/3541958-Software%20&%20Firmware/?docType=1555893-Firmware&language=en_GB-English&sortByField=Popularity"
     folder = 'File_system'
-    se_firmaware_parser(url, folder)
+    total_fw = se_get_total_firmware_count(url)
+    api_url = "https://www.se.com/ww/en/download/doc-group-type/3541958-Software%20&%20Firmware/resultViewCahnge/resultListAjax"
+    raw_fw_list = get_firmware_data_using_api(api_url, total_fw, 50) #50 is max fw_per_page
+    print(raw_fw_list[0])
+    
+    #se_firmaware_parser(url, folder)
