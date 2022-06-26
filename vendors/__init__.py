9a48bfd19576363b0a71ccde58c14adb0d087b50
Tariq Magsi <tariqmagsi125@gmail.com>
2022-06-27 01:26:01 +0500

    updated runner

--- vendors/__init__.py
+++ vendors/__init__.py
@@ -14,15 +14,17 @@ with open('config/config.json', 'r') as f:
 vendors_path = 'vendors'
 
 def job(file):
-    os.system("python vendors/" + file)
+    print(file)
+    # os.system("python vendors/" + file)
 
 def scanner(mod_name, file):
-    if file.split('.')[0] in data:
-        if file.split('.')[0] == mod_name:
+    print(mod_name)
+    if file.split('.')[0] == mod_name:
+        if file.split('.')[0] in data:
             logger.info(f"Starting {file} downloader ...")
             schedule.every(data[file.split('.')[0]]['interval']).minutes.do(job, file)
-    else:
-        schedule.every(data['default']['interval']).minutes.do(job, file)
+        else:
+            schedule.every(data['default']['interval']).minutes.do(job, file)
 
 def mod_runner(mod_name):
     for file in os.listdir(vendors_path):
@@ -37,7 +39,9 @@ def mod_runner(mod_name):
 def runner(num_threads=2, skip_modules=[]):
     logger.info(f"Enabled modules:")
     for file in os.listdir(vendors_path):
-        logger.info(f"{file.split('.')[0]}")
+        if file.endswith(".py") and file.split('.')[0] in data and file != "__init__.py":
+            if file.split('.')[0] in skip_modules:
+                logger.info(f"{file.split('.')[0]}")
     mods_need_run = []
     for file in os.listdir(vendors_path):
         if file.endswith(".py") and file.split('.')[0] in data and file != "__init__.py":
@@ -45,6 +49,6 @@ def runner(num_threads=2, skip_modules=[]):
                 logger.info(f"Skipping {file.split('.')[0]}")
                 continue
             mods_need_run.append(file.split('.')[0])
-
+    print(mods_need_run)
     with Pool(processes=num_threads) as pool:
         pool.map(mod_runner, mods_need_run)
