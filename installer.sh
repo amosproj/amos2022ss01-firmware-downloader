9190700b5bf8e94b560bb6c7f0f899bbfffe8b79
Tariq Magsi <tariqmagsi125@gmail.com>
2022-06-28 01:33:39 +0500

    installer.sh added
    Signed-off-by: Tariq Magsi <tariqmagsi125@gmail.com>

---
+++ installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RANDOM_PW=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 10 | head -n 1)
+
+# first install
+pip install pipreqs
+pipreqs path/to/project # Generate requirements.txt file
+# There is now a requirements.txt file in the project folder which will have dependencies.
+# to install a requirements.txt file:
+pip install -r requirements.txt
+
+echo "Packages Installed Successfully"
+
+USER_NAME="firmware.downloader"
+PASSWORD=$RANDOM_PW
+
+echo "{username:\"${USER_NAME}\", password:\"${PASSWORD}\"}" > config/auth_config.json
+
+echo "DB Username="$USER_NAME
+echo "DB Password="$PASSWORD
+echo "Config File Created Successfully For DB Username and Password"
