e3d787ff30d6c720c0eccc729116d8414e2e7dca
ariffazeel99 <arif.fazeel99@gmail.com>
2022-07-07 23:52:59 +0200

    test cases for check duplicates

--- unit_tests/test_check_duplicates.py
+++ unit_tests/test_check_duplicates.py
@@ -2,33 +2,60 @@ import os
 import sys
 import unittest
 sys.path.append(os.path.abspath(os.path.join('.', '')))
-import sqlite3
+#import sqlite3
 from utils.check_duplicates import check_duplicates
 from utils.database import Database
 
-
-def fetch_data():
-    db_name = "firmwaredatabase.db"
-    
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    try:
-        cursor.execute("select * from FWDB")
-    except sqlite3.Error as er:
-        print('SQLite error:%s' % (' '.join(er.args)))
-
-    data_list = cursor.fetchall()
-    print(data_list)
-    conn.close()
 #https://sqliteviewer.flowsoft7.com/
 class Unit_Case_Test(unittest.TestCase):
     def test_if_check_duplicates_working_correctly(self):
         temp_data = {
-            'Manufacturer': 'GE',
-            'Fwfilename': 'orbit-bkrc-9_2_2.mpk',
-            'Modelname': 'orbit-bkrc-9_2_2.mpk',
-            'Version': '',
-            'Type': '',
+            'Fwfileid': 'c',
+            'Fwfilename': 'x',
+            'Manufacturer': 'd',
+            'Modelname': 'SZ-d',
+            'Version': '1',
+            'Type': 'Router',
+            'Releasedate': '2022-05-31',
+            'Checksum': 'None',
+            'Embatested': 'Yes',
+            'Embalinktoreport': 'None',
+            'Embarklinktoreport': 'https://xyz.com',
+            'Fwdownlink': 'https://google.com',
+            'Fwfilelinktolocal': './xyz/abc.tar',
+            'Fwadddata': 'some long sentence'
+        }
+        self.assertFalse(check_duplicates(temp_data, 'firmwaredatabase.db'), msg="Data not exist")
+
+    def test_if_check_duplicates_working_correctly_with_data_inserted(self):
+        db_name = 'firmwaredatabase.db'
+        db = Database()
+        if db_name not in os.listdir('.'):
+            db.create_table()
+        # Create a function for selenium output in dict format and return the dict. Pass it in the next line to insert the data
+        db.insert_data(dbdictcarrier={
+            'Fwfileid': 'FILE',
+            'Fwfilename': 'Siemens ABC firmware',
+            'Manufacturer': 'Siemens',
+            'Modelname': 'SZ-100',
+            'Version': '1.2.3',
+            'Type': 'Router',
+            'Releasedate': '2022-05-31',
+            'Checksum': 'None',
+            'Embatested': 'Yes',
+            'Embalinktoreport': 'None',
+            'Embarklinktoreport': 'https://xyz.com',
+            'Fwdownlink': 'https://google.com',
+            'Fwfilelinktolocal': './xyz/abc.tar',
+            'Fwadddata': 'some long sentence'
+        })
+        temp_data = {
+            'Fwfileid': 'FILE',
+            'Fwfilename': 'Siemens ABC firmware',
+            'Manufacturer': 'Siemens',
+            'Modelname': 'SZ-100',
+            'Version': '1.2.3',
+            'Type': 'Router',
             'Releasedate': '2022-05-31',
             'Checksum': 'None',
             'Embatested': 'Yes',
@@ -40,31 +67,5 @@ class Unit_Case_Test(unittest.TestCase):
         }
         self.assertTrue(check_duplicates(temp_data, 'firmwaredatabase.db'), msg="Data not exist")
 
-    def test_if_check_duplicates_working_correctly_with_data_inserted(self):
-        temp_data = {
-            'Manufacturer': 'GE',
-            'Fwfilename': 'orbit-bkrc-9_2_2.mpk',
-            'Modelname': 'orbit-bkrc-9_2_2.mpk',
-            'Version': '',
-            'Type': '',
-            'Releasedate': '2022-05-31',
-            'Checksum': 'None',
-            'Embatested': 'Yes',
-            'Embalinktoreport': 'None',
-            'Embarklinktoreport': 'https://xyz.com',
-            'Fwdownlink': 'https://google.com',
-            'Fwfilelinktolocal': './xyz/abc.tar',
-            'Fwadddata': 'some long sentence'
-        }
-        db_name = 'firmwaredatabase.db'
-        db = Database()
-        # Create a function for selenium output in dict format and return the dict. Pass it in the next line to insert the data
-        db.insert_data(dbdictcarrier=temp_data)
-        
-        self.assertTrue(check_duplicates(temp_data, db_name), msg="Data not exist")
-
-
 if __name__ == "__main__":
-    fetch_data()
     unittest.main()
-
