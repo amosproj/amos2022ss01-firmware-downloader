cd185bd5f825876519ae9541b653cf78303a1788
Tariq Magsi <tariqhussainmagsi@faustud-010-021-146-137.pool.uni-erlangen.de>
2022-07-10 16:26:02 +0200

    schneider_electric_tests.py linting updated
    Signed-off-by: Tariq Magsi <tariqhussainmagsi@faustud-010-021-146-137.pool.uni-erlangen.de>

--- unit_tests/schneider_electric_tests.py
+++ unit_tests/schneider_electric_tests.py
@@ -4,11 +4,10 @@ import sqlite3
 import unittest
 import json
 sys.path.append(os.path.abspath(os.path.join('.', '')))
-from utils.database import Database
 from vendors.schneider_electric import download_single_file
 from utils.check_duplicates import check_duplicates
 
-db_name = "firmwaredatabase.db"
+DB_NAME = "firmwaredatabase.db"
 CONFIG_PATH = os.path.join("config", "config.json")
 DATA={}
 with open(CONFIG_PATH, "rb") as fp:
@@ -16,19 +15,19 @@ with open(CONFIG_PATH, "rb") as fp:
 
 def fetch_data():
     # db connection
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     try:
         cursor.execute("select * from FWDB WHERE Manufacturer='schneider_electric'")
-    except sqlite3.Error as er:
-        print('SQLite error:%s' % (' '.join(er.args)))
+    except sqlite3.Error as er_:
+        print('SQLite error:%s' % (' '.join(er_.args)))
 
     data_list = cursor.fetchall()
     print(data_list)
     conn.close()
 
 
-class Unit_Case_Test(unittest.TestCase):
+class SchneiderUnitTest(unittest.TestCase):
     def test_if_download_working_correctly(self):
             gt_url = "https://download.schneider-electric.com/files?p_enDocType=Firmware&p_File_Name=PM5560_PM5563_V2.7.4_Release.zip&p_Doc_Ref=PM5560_PM5563_V2.7.4_Release"
             dest = DATA['file_paths']['dowload_test_files_path']
@@ -40,9 +39,10 @@ class Unit_Case_Test(unittest.TestCase):
                 os.remove(gt_file_path)
             download_single_file(gt_url, gt_file_path)
             self.assertTrue(os.path.exists(gt_file_path), msg="Path not exists")
+            fetch_data()
 
-    def test_if_data_entered_in_db_for_schneider_electric(self):
-            conn = sqlite3.connect(db_name)
+    def test_if_data_entered_in_db(self):
+            conn = sqlite3.connect(DB_NAME)
             curs = conn.cursor()
             select_command = "select * from FWDB WHERE Manufacturer='schneider_electric'"
             curs.execute(select_command)
@@ -57,9 +57,9 @@ class Unit_Case_Test(unittest.TestCase):
             'Modelname': file_name,
             'Version': '',
         }
-        self.assertFalse(check_duplicates(data, db_name), msg="Image didn't downloaded")
+        self.assertFalse(check_duplicates(data, DB_NAME), msg="Image didn't downloaded")
+        fetch_data()
 
 
 if __name__ == "__main__":
-    fetch_data()
     unittest.main()
