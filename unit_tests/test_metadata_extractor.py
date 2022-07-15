4772fb643d9c77f2b6be46606982b721778a3c83
udayvarun <uday.varun.kothapalli@fau.de>
2022-07-15 18:24:32 +0200

    Unittest case file is created for metadata_extractor.py

---
+++ unit_tests/test_metadata_extractor.py
@@ -0,0 +1,18 @@
+from utils.metadata_extractor import metadata_extractor
+import os
+import unittest
+
+
+class MetadataExtractor(unittest.TestCase):
+    def test_metadata_extractor(self):
+        path = os.getcwd()
+        properties = metadata_extractor(r"{}\test_ge.py".format(path))
+        self.assertIn("File Name", properties.keys())
+        self.assertIn("Creation Date", properties.keys())
+        self.assertIn("Last Edit Date", properties.keys())
+        self.assertIn("File Size", properties.keys())
+        self.assertIn("Hash Value", properties.keys())
+
+
+if __name__ == "__main__":
+    unittest.main()
