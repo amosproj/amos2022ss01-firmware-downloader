c8ecb7ce84e51a8c95a96e826577974ca4b4c5ce
udayvarun <uday.varun.kothapalli@fau.de>
2022-07-15 18:19:33 +0200

    Refactored metadata_extractor.py

--- utils/metadata_extractor.py
+++ utils/metadata_extractor.py
@@ -1,29 +1,32 @@
-import os, hashlib
-from datetime import datetime
+import hashlib
+import os
+import time
 
 
-def get_file_metadata(file_name):
-    creation_date = datetime.fromtimestamp(os.path.getctime(file_name))
-    last_edit_date = datetime.fromtimestamp(os.path.getmtime(file_name))
-    file_size = os.path.getsize(file_name)
+def get_file_metadata(file_path):
+    # It will return the properties of the file by using file path
+    file_name = os.path.basename(file_path)
+    creation_date = time.ctime(os.path.getctime(file_path))
+    last_edit_date = time.ctime(os.path.getmtime(file_path))
+    file_size = os.path.getsize(file_path)
     return file_name, creation_date, last_edit_date, file_size
 
 
-def mesh_value(file_name):
-    with open(file_name, "rb") as file:
+def get_hash_value(file_path):
+    # Returns the hash value of the file using file path
+    with open(file_path, "rb") as file:
         file_hash = hashlib.md5()
         if file.read(8192):
             chunk = file.read(8192)
             file_hash.update(chunk)
         file.close()
-        print("Hash Value: ", file_hash.hexdigest())
+        return file_hash.hexdigest()
 
 
-if __name__ == '__main__':
-    # Replace path to required directory in filenames, currently using current directory
-    filenames = os.listdir('.')
-    for filename in filenames:
-        metadata = get_file_metadata(filename)
-        print("File Name: ", metadata[0], "\nCreation Date: ", metadata[1],
-              "\nLast Edit Date: ", metadata[2], "\nFile Size: ", metadata[3])
-        mesh_value(filename)
+def metadata_extractor(file_path):
+    # Returns metadata of the file using file path in dictionary
+    metadata = get_file_metadata(file_path)
+    hash_value = get_hash_value(file_path)
+    properties = {"File Name": metadata[0], "Creation Date": metadata[1], "Last Edit Date": metadata[2],
+                  "File Size": str(metadata[3]) + ' bytes', "Hash Value": hash_value}
+    return properties
