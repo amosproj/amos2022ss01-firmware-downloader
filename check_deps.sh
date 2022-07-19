cf690e613a6ed35f7b9f65254468488feabf40f5
AliAsghar01 <alan.asghar.awan@gmail.com>
2022-07-19 19:21:43 +0000

    fix check dep script

--- check_deps.sh
+++ check_deps.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 is_python_installed=0
-
 which python;
 if [ $? = 0 ] ; then
        echo "Python is installed"
-	echo $(python --version)
+        python --version
+        is_python_installed=1
+fi
+which python3;
+if [ $? = 0 ] ; then
+	echo "Python is installed"
+	python3 --version
 	is_python_installed=1
-fi	
+fi
 if [ $is_python_installed = 0 ] ; then
-    echo "Python installation not found"
-    echo "installing python"
-    apt-get update
-    apt-get install software-properties-common
-    add-apt-repository ppa:deadsnakes/ppa
-    apt-get install python3.8
+	echo "Python installation not found"
+	echo "installing python"
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update
+	apt-get install -y software-properties-common
+	add-apt-repository ppa:deadsnakes/ppa
+	apt-get install -y python3.8
 fi
